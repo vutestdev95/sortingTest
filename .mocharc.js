772179da833abe01761f83d399feccab7712dcee
Nam Hoang Le <nam.hoang.le@mgm-tp.com>
2023-01-07 09:52:10 +0700

    Init

---
+++ .mocharc.js
@@ -0,0 +1,8 @@
+"use strict";
+module.exports = {
+	spec: "./src/test/**/*.test.js",
+	timeout: 60000,
+	ui: "bdd",
+	"forbid-only": true,
+	sort: true,
+};
---
+++ src/test/sum.test.js
@@ -0,0 +1,10 @@
+const {expect} = require("chai");
+const {sum} = require("../main/sum");
+
+describe("sum", () => {
+    describe("given two number", () => {
+        it("should return the sum of the two numbers", () => {
+            expect(sum(1, 2)).to.be.equal(3);
+        })
+    })
+})
