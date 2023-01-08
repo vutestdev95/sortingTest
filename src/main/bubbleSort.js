3fe8c36b0ddcdb2be6397d0c8b0934131904c947
macOS <macos@macOSs-MacBook-Pro.local>
2023-01-08 14:58:36 +0700

    weqweqweqw

--- src/main/bubbleSort.js
+++ src/main/bubbleSort.js
@@ -1,18 +1,20 @@
-function checkIsArray (array) {
-    return Array.isArray(array)
+function checkIsArray(array) {
+  return Array.isArray(array);
 }
-function bubbleSort (arrOfNumber) {
-    if(!checkIsArray(arrOfNumber)) return false
-    for(let i = 0; i < arrOfNumber.length; i++ ){
-        for (let j = 0; j < arrOfNumber.length - 1; j++){
-            if(arrOfNumber[j]>arrOfNumber[j+1]){
-                let cloneVariable = arrOfNumber[j];
-                arrOfNumber[j] = arrOfNumber[j+1];
-                arrOfNumber[j+1] = cloneVariable;
-            }
-        }
+function bubbleSort(arrOfNumber) {
+  if (!checkIsArray(arrOfNumber)) {
+    return false;
+  }
+  for (let i = 0; i < arrOfNumber.length; i++) {
+    for (let j = 0; j < arrOfNumber.length - 1; j++) {
+      if (arrOfNumber[j] > arrOfNumber[j + 1]) {
+        let cloneVariable = arrOfNumber[j];
+        arrOfNumber[j] = arrOfNumber[j + 1];
+        arrOfNumber[j + 1] = cloneVariable;
+      }
     }
-    return arrOfNumber;
+  }
+  return arrOfNumber;
 }
-console.log(bubbleSort([2,1,3,4,5,6,7,8,9,0]));
-module.exports = {bubbleSort}
+console.log(bubbleSort([2, 1, 3, 4, 5, 6, 7, 8, 9, 0]));
+module.exports = { bubbleSort };
--- src/test/bubbleSort.test.js
+++ src/test/bubbleSort.test.js
@@ -1,10 +1,15 @@
-const {expect} = require("chai");
-const {bubbeSort} = require("../main/bubbleSort");
+const { expect } = require("chai");
+const { bubbleSort } = require("../main/bubbleSort");
 
 describe("bubbleSort", () => {
-    describe("Give an array", () => {
-        it("should return the sum of the sorted array", () => {
-            expect(bubbeSort([1,2,3,6,4,90,11,33,5])).to.be.equal([1,2,3,4,5,6,11,33,90]);
-        })
-    })
-})
+  describe("Give an array", () => {
+    it("should return the sum of the sorted array", () => {
+      expect(bubbleSort([1, 2, 3, 6, 4, 90, 11, 33, 5])).to.be.deep.equal([
+        1, 2, 3, 4, 5, 6, 11, 33, 90,
+      ]);
+      expect(bubbleSort([1, 2, 3, 6, 4, 90, 11, 33, 5])).to.be.deep.equal([
+        1, 2, 3, 4, 5, 6, 11, 33, 90,
+      ]);
+    });
+  });
+});
