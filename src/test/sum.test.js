4cd7448efbee4b452c2419d76d5c0eb979b3268f
Nam Hoang Le <nam.hoang.le@mgm-tp.com>
2023-01-08 14:57:57 +0700

    Add more test

--- src/test/sum.test.js
+++ src/test/sum.test.js
@@ -5,6 +5,7 @@ describe("sum", () => {
   describe("given two number", () => {
     it("should return the sum of the two numbers", () => {
       expect(sum(1, 2)).to.be.equal(3);
+      expect(sum(1, 3)).to.be.equal(4);
     });
   });
 });
